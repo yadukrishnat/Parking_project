1a9cb7fc7182d4756e5a998f1ebe7093b337e53f
CRS <yadukrishna2240@gmail.com>
2025-11-17 12:51:27 +0530

    added user_me endpoint

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,31 @@
 package com.example.demo.controller;
 
-public class UserController {
-}
+import com.example.demo.JavaUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+    @RestController
+    @RequestMapping("/api/user")
+    public class UserController {
+
+        private final JavaUtils javaUtils;
+
+        public UserController(JavaUtils javaUtils) {
+            this.javaUtils = javaUtils;
+        }
+
+        @GetMapping("/me")
+        public String getMyEmail(@RequestHeader("Authorization") String authHeader) {
+            String token = authHeader.substring(7);
+            String email = javaUtils.extractUsername(token);
+            return email;
+        }
+    }
+
+
+
+
+
